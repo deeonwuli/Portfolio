49346d51e7b8c4716f9951d968cd768ab2ff7935
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2020-04-25 00:21:28 +0100

    so many changes and additions

--- NinjaGame/game.js
+++ NinjaGame/game.js
@@ -55,8 +55,9 @@ window.onload = function () {
     type: Phaser.AUTO,
     width: 1334,
     height: 750,
-    scene: [preloadGame, playGame, GameOver],
-    backgroundColor: 0x87CEFA,
+    scene: [preloadGame, playGame, GameOver, Starfield, Highscore, InputPanel],
+    backgroundColor: 0x000000,
+    pixelArt: true,
 
     // physics settings
     physics: {
@@ -83,8 +84,8 @@ class preloadGame extends Phaser.Scene {
 
     // player is a sprite sheet made by 24x48 pixels
     this.load.spritesheet('player', 'assets/player.png', {
-      frameWidth: 24,
-      frameHeight: 48
+      frameWidth: 50,
+      frameHeight: 60
     })
 
     // enemy is a sprite sheet made by 24x48 pixels
@@ -159,7 +160,15 @@ class playGame extends Phaser.Scene {
     super('PlayGame')
   }
 
+  preload () {
+    this.load.image('score', 'assets/score.png')
+  }
+
   create () {
+    this.scene.stop('Starfield')
+    this.scene.stop('InputPanel')
+    this.scene.stop('Highscore')
+
     // group with all active mountains.
     this.mountainGroup = this.add.group()
 
@@ -238,7 +247,7 @@ class playGame extends Phaser.Scene {
         this.player.anims.play('run')
       } else {
         score += 0.05
-        scoreText.setText('Score: ' + Math.round(score))
+        scoreText.setText(Math.round(score))
       }
     }, null, this)
 
@@ -255,7 +264,7 @@ class playGame extends Phaser.Scene {
           score += 10
           this.coinGroup.killAndHide(coin)
           this.coinGroup.remove(coin)
-          scoreText.setText('Score: ' + Math.round(score))
+          scoreText.setText(Math.round(score))
         }
       })
     }, null, this)
@@ -272,9 +281,10 @@ class playGame extends Phaser.Scene {
     // checking for input
     this.input.on('pointerdown', this.jump, this)
 
-    scoreText = this.add.text(25, 25, 'Score: 0', {
-      font: 'bold 30px Tahoma',
-      fill: '#000000'
+    let txt = this.add.image(25, 25, 'score').setOrigin(0, 0)
+    scoreText = this.add.text(220, 8, ' 0', {
+      font: 'bold 50px Arial',
+      fill: '#fff'
     })
     scoreText.setScrollFactor(0)
   }
@@ -473,6 +483,8 @@ class GameOver extends Phaser.Scene {
   }
 
   preload () {
+    this.load.image('gameover', 'assets/gameover.png')
+    this.load.image('score', 'assets/score.png')
     this.load.image('background', 'assets/over.png')
     this.load.image('leaderboard', 'assets/leaderboard.png')
     this.load.image('tryAgain', 'assets/tryagain.png')
@@ -483,20 +495,22 @@ class GameOver extends Phaser.Scene {
     let bg = this.add.image(150, 100, 'background').setOrigin(0)
 
     // Title
-    let title = this.add.text(350, 200, 'Game Over!', {
-      font: 'bold 100px Tahoma',
-      fill: '#ffffff'
-    })
+    let title = this.add.image(300, 200, 'gameover').setOrigin(0, 0)
+    title.setScale(2.5)
 
     // score text
-    let txt_score = this.add.text(475, 300, '\nScore: ' + Math.round(score), {
-      font: 'bold 70px Tahoma',
+    let txt_score = this.add.image(430, 350, 'score').setOrigin(0, 0)
+    txt_score.setScale(2)
+
+    // score
+    let scores = this.add.text(800, 335, Math.round(score), {
+      font: 'bold 70px Arial',
       fill: '#ffffff'
     })
 
     // Buttons
-    let lb = this.add.image(750, 500, 'leaderboard').setOrigin(0)
-    let ta = this.add.image(400, 500, 'tryAgain').setOrigin(0)
+    let lb = this.add.image(650, 500, 'leaderboard').setOrigin(0)
+    let ta = this.add.image(300, 500, 'tryAgain').setOrigin(0)
 
     lb.setInteractive({ useHandCursor: true })
     ta.setInteractive({ useHandCursor: true })
@@ -506,8 +520,8 @@ class GameOver extends Phaser.Scene {
   }
 
   clickLeaderboard () {
-    this.scene.switch('PlayGame')
-    score = 0
+    this.scene.start('Highscore')
+    this.scene.launch('Starfield')
   }
 
   clickTryAgain () {
@@ -580,6 +594,270 @@ fetch('https://penguinjaleaderboard-a704.restdb.io/rest/score', {
 
 function isMobileDevice () {
   return (typeof window.orientation !== 'undefined') || (navigator.userAgent.indexOf('IEMobile') !== -1)
+}
+
+// Leaderboard display
+class InputPanel extends Phaser.Scene {
+  constructor () {
+    super({
+      key: 'InputPanel',
+      active: false
+    })
+    this.chars = [
+      [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' ],
+      [ 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T' ],
+      [ 'U', 'V', 'W', 'X', 'Y', 'Z', '.', '-', '<', '>' ]
+    ]
+    this.cursor = new Phaser.Math.Vector2()
+
+    this.text
+    this.block
+
+    this.name = ''
+    this.charLimit = 3
+  }
+
+  create () {
+    let text = this.add.bitmapText(350, 150, 'arcade', 'ABCDEFGHIJ\n\nKLMNOPQRST\n\nUVWXYZ.-')
+
+    text.setLetterSpacing(20)
+    text.setInteractive()
+
+    this.add.image(text.x + 430, text.y + 148, 'rub')
+    this.add.image(text.x + 482, text.y + 148, 'end')
+
+    this.block = this.add.image(text.x - 10, text.y - 2, 'block').setOrigin(0)
+
+    this.text = text
+
+    this.input.keyboard.on('keyup_LEFT', this.moveLeft, this)
+    this.input.keyboard.on('keyup_RIGHT', this.moveRight, this)
+    this.input.keyboard.on('keyup_UP', this.moveUp, this)
+    this.input.keyboard.on('keyup_DOWN', this.moveDown, this)
+    this.input.keyboard.on('keyup_ENTER', this.pressKey, this)
+    this.input.keyboard.on('keyup_SPACE', this.pressKey, this)
+    this.input.keyboard.on('keyup', this.anyKey, this)
+
+    text.on('pointermove', this.moveBlock, this)
+    text.on('pointerup', this.pressKey, this)
+
+    this.tweens.add({
+      targets: this.block,
+      alpha: 0.2,
+      yoyo: true,
+      repeat: -1,
+      ease: 'Sine.easeInOut',
+      duration: 350
+    })
+  }
+
+  moveBlock (pointer, x, y) {
+    let cx = Phaser.Math.Snap.Floor(x, 52, 0, true)
+    let cy = Phaser.Math.Snap.Floor(y, 64, 0, true)
+    let char = this.chars[cy][cx]
+
+    this.cursor.set(cx, cy)
+
+    this.block.x = this.text.x - 10 + (cx * 52)
+    this.block.y = this.text.y - 2 + (cy * 64)
+  }
+
+  moveLeft () {
+    if (this.cursor.x > 0) {
+      this.cursor.x--
+      this.block.x -= 52
+    } else {
+      this.cursor.x = 9
+      this.block.x += 52 * 9
+    }
+  }
+
+  moveRight () {
+    if (this.cursor.x < 9) {
+      this.cursor.x++
+      this.block.x += 52
+    } else {
+      this.cursor.x = 0
+      this.block.x -= 52 * 9
+    }
+  }
+
+  moveUp () {
+    if (this.cursor.y > 0) {
+      this.cursor.y--
+      this.block.y -= 64
+    } else {
+      this.cursor.y = 2
+      this.block.y += 64 * 2
+    }
+  }
+
+  moveDown () {
+    if (this.cursor.y < 2) {
+      this.cursor.y++
+      this.block.y += 64
+    } else {
+      this.cursor.y = 0
+      this.block.y -= 64 * 2
+    }
+  }
+
+  anyKey (event) {
+    //  Only allow A-Z . and -
+    let code = event.keyCode
+    if (code === Phaser.Input.Keyboard.KeyCodes.PERIOD) {
+      this.cursor.set(6, 2)
+      this.pressKey()
+    } else if (code === Phaser.Input.Keyboard.KeyCodes.MINUS) {
+      this.cursor.set(7, 2)
+      this.pressKey()
+    } else if (code === Phaser.Input.Keyboard.KeyCodes.BACKSPACE || code === Phaser.Input.Keyboard.KeyCodes.DELETE) {
+      this.cursor.set(8, 2)
+      this.pressKey()
+    } else if (code >= Phaser.Input.Keyboard.KeyCodes.A && code <= Phaser.Input.Keyboard.KeyCodes.Z) {
+      code -= 65
+
+      let y = Math.floor(code / 10)
+      let x = code - (y * 10)
+
+      this.cursor.set(x, y)
+      this.pressKey()
+    }
+  }
+
+  pressKey () {
+    let x = this.cursor.x
+    let y = this.cursor.y
+    let nameLength = this.name.length
+
+    this.block.x = this.text.x - 10 + (x * 52)
+    this.block.y = this.text.y - 2 + (y * 64)
+
+    if (x === 9 && y === 2 && nameLength > 0) {
+      //  Submit
+      this.events.emit('submitName', this.name)
+    } else if (x === 8 && y === 2 && nameLength > 0) {
+      //  Rub
+      this.name = this.name.substr(0, nameLength - 1)
+
+      this.events.emit('updateName', this.name)
+    } else if (this.name.length < this.charLimit) {
+      //  Add
+      this.name = this.name.concat(this.chars[y][x])
+
+      this.events.emit('updateName', this.name)
+    }
+  }
+}
+
+class Starfield extends Phaser.Scene {
+  constructor () {
+    super({
+      key: 'Starfield',
+      active: true
+    })
+
+    this.stars
+
+    this.distance = 300
+    this.speed = 250
+
+    this.max = 500
+    this.xx = []
+    this.yy = []
+    this.zz = []
+  }
+
+  preload () {
+    this.load.image('star', 'assets/star.png')
+  }
+
+  create () {
+    //  Do this, otherwise this Scene will steal all keyboard input
+    this.input.keyboard.enabled = false
+
+    this.stars = this.add.blitter(150, 150, 'star')
+
+    for (let i = 0; i < this.max; i++) {
+      this.xx[i] = Math.floor(Math.random() * 800) - 400
+      this.yy[i] = Math.floor(Math.random() * 600) - 300
+      this.zz[i] = Math.floor(Math.random() * 1700) - 100
+
+      let perspective = this.distance / (this.distance - this.zz[i])
+      let x = 400 + this.xx[i] * perspective
+      let y = 300 + this.yy[i] * perspective
+
+      this.stars.create(x, y)
+    }
+  }
+
+  update (time, delta) {
+    for (let i = 0; i < this.max; i++) {
+      let perspective = this.distance / (this.distance - this.zz[i])
+      let x = 400 + this.xx[i] * perspective
+      let y = 300 + this.yy[i] * perspective
+
+      this.zz[i] += this.speed * (delta / 1000)
+
+      if (this.zz[i] > 300) {
+        this.zz[i] -= 600
+      }
+
+      let bob = this.stars.children.list[i]
+
+      bob.x = x
+      bob.y = y
+    }
+  }
+}
+
+class Highscore extends Phaser.Scene {
+  constructor () {
+    super({
+      key: 'Highscore',
+      active: false
+    })
+    this.playerText
+  }
+
+  preload () {
+    this.load.image('block', 'assets/block.png')
+    this.load.image('rub', 'assets/rub.png')
+    this.load.image('end', 'assets/end.png')
+
+    this.load.bitmapFont('arcade', 'assets/arcade.png', 'assets/arcade.xml')
+  }
+
+  create () {
+    this.add.bitmapText(350, 350, 'arcade', 'RANK  SCORE   NAME').setTint(0xff00ff)
+    this.add.bitmapText(350, 400, 'arcade', '1ST   50000').setTint(0xff0000)
+
+    this.playerText = this.add.bitmapText(830, 400, 'arcade', '').setTint(0xff0000)
+
+    //  Do this, otherwise this Scene will steal all keyboard input
+    this.input.keyboard.enabled = false
+
+    this.scene.launch('InputPanel')
+
+    let panel = this.scene.get('InputPanel')
+
+    //  Listen to events from the Input Panel scene
+    panel.events.on('updateName', this.updateName, this)
+    panel.events.on('submitName', this.submitName, this)
+  }
+
+  submitName () {
+    this.scene.stop('InputPanel')
+
+    this.add.bitmapText(350, 450, 'arcade', '2ND   40000    ANT').setTint(0xff8200)
+    this.add.bitmapText(350, 500, 'arcade', '3RD   30000    .-.').setTint(0xffff00)
+    this.add.bitmapText(350, 550, 'arcade', '4TH   20000    BOB').setTint(0x00ff00)
+    this.add.bitmapText(350, 600, 'arcade', '5TH   10000    ZIK').setTint(0x00bfff)
+  }
+
+  updateName (name) {
+    this.playerText.setText(name)
+  }
 }
 
 /* function checkOrientation (orientation) {
